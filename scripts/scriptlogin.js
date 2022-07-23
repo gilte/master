928e4674e114bf6832c8230838eab7a8bcae5cfa
Eliel <75944151+gilte@users.noreply.github.com>
2022-07-22 23:29:32 -0300

    Add files via upload

--- scripts/scriptlogin.js
+++ scripts/scriptlogin.js
@@ -1,1 +1,20 @@
 
+
+    var loginCadastrado = "user123";
+    var senhaCadastrada = "123456";
+
+    contador = 1;
+    while(contador <=99999999){
+    var loginInformado = prompt("Informe seu login");
+    var senhaInformada = prompt("Informe sua senha");
+
+    if( loginCadastrado == loginInformado && senhaCadastrada == senhaInformada ) {
+
+        alert("Bem-vindo ao sistema " + loginInformado);
+        contador = 100000000;
+    } else {
+        alert("Login inválido. Tente novamente");
+        contador = contador + 1;
+        }
+
+    }
